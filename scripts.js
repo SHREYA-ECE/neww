3fcf0b99087f4e7186a2bad7bcbd67ccc8cfa023
SHREYA-ECE <shreya210405@gmail.com>
2024-09-16 00:17:35 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    document.getElementById('messagePopup').style.display = 'block';
+}
+
+function hideMessage() {
+    document.getElementById('messagePopup').style.display = 'none';
+}
